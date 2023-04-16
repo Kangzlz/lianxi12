83c75e3c0b287ef514f5078dc23dcc0d7b4dc31a
Kangzlz <127727428+Kangzlz@users.noreply.github.com>
2023-04-16 21:09:15 +0800

    第四次提交

---
+++ lianxi13-4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<time.h>
+int main(void){
+    FILE *fp;
+    char name[100];
+    double height,weight;
+    puts("请输入姓名 身高 体重。");
+    scanf("%s %lf %lf",name,&height,&weight);
+    if((fp=fopen("dd_dat","w"))==NULL)
+    printf("\a无法打开文件。");
+    else{
+        puts("写出姓名 身高 体重。");
+        fprintf(fp,"%-15s %6.1f %6.1f\n",name,height,weight);
+        fclose(fp);
+    }
+    return 0;
+}
