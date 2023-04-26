c03928217c3053a596736dec7ccd16d8450bb17d
Kangzlz <127727428+Kangzlz@users.noreply.github.com>
2023-04-26 16:49:10 +0800

    9

---
+++ lianxi13-13.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(void){
+    int ch;
+    FILE *sfp,*dfp;
+    char sname[FILENAME_MAX],dname[FILENAME_MAX];
+    printf("原文件：");scanf("%s",sname);
+    printf("目标文件：");scanf("%s",dname);
+    if((sfp=fopen(sname,"rb"))==NULL)
+    printf("\a文件打开失败。\n");
+    else{
+        if((dfp=fopen(dname,"wb"))==NULL)
+        printf("\a文件打开失败。\n");
+        else{
+            fread(&ch,1,1,sfp);
+            fwrite(&ch,1,1,dfp);
+            fclose(dfp);
+        }
+        fclose(sfp);
+    }
+    return 0;
+}
